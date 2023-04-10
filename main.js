c9bd0f8f6d59e991d28bc06db0f293b96811e8a5
omarisayed <osayedunoff@gmail.com>
2023-04-10 15:13:43 +0300

    Improved validation by adding regex.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let successIcon = document.getElementsByClassName('success-icon');
 let failureIcon = document.getElementsByClassName('failure-icon');
 
 submitBtn.addEventListener('click', (e) =>{
-    e.preventDefault(); //stop flickering/flashing behavior of error message. Make it stay on its div without disappearing.
+    e.preventDefault(); //to stop flickering/flashing behavior of error message. Make it stay without disappearing.
 
     fName();
     lName();
@@ -21,8 +21,9 @@ submitBtn.addEventListener('click', (e) =>{
 });
 
 function fName(){
-    if (firstName.value === ''){
-        errorMessage[0].innerHTML = "This field is required";
+    let validFname = (/^[A-Za-z]+$/);
+    if (firstName.value === '' || firstName.value.match(validFname) === null){  
+        errorMessage[0].innerHTML = "Please enter a valid name";
         failureIcon[0].style.opacity = "1";
         successIcon[0].style.opacity = "";
 
@@ -34,8 +35,9 @@ function fName(){
 }
 
 function lName(){
-    if (lastName.value === ''){
-        errorMessage[1].innerHTML = "This field is required";
+    let validLname = (/^[A-Za-z]+$/);
+    if (lastName.value === '' || lastName.value.match(validLname) === null){
+        errorMessage[1].innerHTML = "Please enter a valid name";
         failureIcon[1].style.opacity = "1";
         successIcon[1].style.opacity = "";
 
@@ -48,36 +50,43 @@ function lName(){
 
 
 function eMail(){
-    if (email.value === ''){
-        errorMessage[2].innerHTML = "Please enter a valid email in the format of user@domain.com.";
+    let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email.value === ''|| email.value.match(validEmail) === null){
+        errorMessage[2].innerHTML = "Please enter a valid email in the format of user@domain.com";
         failureIcon[2].style.opacity = "1";
         successIcon[2].style.opacity = "";
-
     }else{
         errorMessage[2].innerHTML = "";
         failureIcon[2].style.opacity = "";
         successIcon[2].style.opacity = "1";
     }
+
 }
 
 
 function telePhone(){
-    if (phone.value === ''){
-        errorMessage[3].innerHTML = "This field is required";
+    let validPhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+    if (phone.value === '' || phone.value.match(validPhone) === null){
+        errorMessage[3].innerHTML = "Please enter a valid phone number of 10 digits";
         failureIcon[3].style.opacity = "1";
         successIcon[3].style.opacity = "";
-
     }else{
         errorMessage[3].innerHTML = "";
         failureIcon[3].style.opacity = "";
         successIcon[3].style.opacity = "1";
     }
+
 }
 
 
+
 function pWord(){
-    if (password.value === ''){
-        errorMessage[4].innerHTML = "This field is required";
+    var validPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,15}$/;
+
+    if (password.value === ''|| password.value.match(validPassword) === null){
+        errorMessage[4].innerHTML = "Please enter a password of 5 to 15 characters, that contains at least one number and one special character";
         failureIcon[4].style.opacity = "1";
         successIcon[4].style.opacity = "";
 
@@ -101,4 +110,7 @@ function confirmPword(){
         successIcon[5].style.opacity = "1";
     }
 }
+
+//--------------------------NOTES----------------------------------------------------------------------------------------------------------------
+//the match() method returns an array of matches if the regular expression matches the string, or null (NOT false) if it doesn't match.
 
